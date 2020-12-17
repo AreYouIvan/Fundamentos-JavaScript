e2d8df0d77c640dd5cf48a24e7ba1b8475567a6c
Ivan Garcia <ivangm180@gmail.com>
2020-12-17 11:24:30 -0600

    objects

---
+++ object.js
@@ -0,0 +1,10 @@
+var miAuto = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    annio: 2020,
+    detalleDelAuto: function(){
+        console.log(`Auto ${this.modelo} ${this.annio}`)
+    }
+};
+
+miAuto.modelo // Puedes pedir un valor especifico agregando un ' . ' entre la variable y la palabra clave
