561c1f9210230fb9be6e2ec2753cdf1a094e6e28
Ivan Garcia <ivangm180@gmail.com>
2020-12-17 11:53:43 -0600

    objeto constructo

---
+++ objects-funcion-cons.js
@@ -0,0 +1,19 @@
+function auto(marca, modelo, annio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+}
+
+var autos = [];
+for(let i = 0 ; i < 2 ; i++) {
+    var marca = prompt("Marca del auto");
+    var modelo = prompt("Modelo del auto");
+    var annio = prompt("Año del auto");
+    autos.push(new auto(marca, modelo, annio));
+}
+
+for(let i = 0 ; i < autos.length ; i++){
+    console.log(autos[i]);
+}
+
+new // Hace referencia a nuestra funcion constructora.
